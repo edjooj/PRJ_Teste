d4a48c205872821b4bd5defd763b17c02eb45ec2
WhoppDev <126715867+WhoppDev@users.noreply.github.com>
2024-01-24 14:40:19 -0300

    Att teste

--- CRIA MUNDO - Teste/Assets/GAME/Scripts/Player/LookatCamera.cs
+++ CRIA MUNDO - Teste/Assets/GAME/Scripts/Player/LookatCamera.cs
@@ -6,10 +6,6 @@ public class LookatCamera : MonoBehaviour
 {
     void Update()
     {
-        transform.LookAt(Camera.main.transform);
 
-        Vector3 eulerAngles = transform.eulerAngles;
-        eulerAngles.y += 180;
-        transform.eulerAngles = eulerAngles;
     }
 }
