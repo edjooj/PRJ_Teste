a67636e224b222f0341cfc3c5090d21f1bede8db
WhoppDev <126715867+WhoppDev@users.noreply.github.com>
2024-01-24 16:02:58 -0300

    Att

--- CRIA MUNDO - Teste/Assets/GAME/Scripts/Player/LookatCamera.cs
+++ CRIA MUNDO - Teste/Assets/GAME/Scripts/Player/LookatCamera.cs
@@ -1,11 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
-public class LookatCamera : MonoBehaviour
+public class LookatCamera : MonoBehaviourPunCallbacks
 {
+    public Camera cameraToLookAt;
+
+    public GameObject playerStatus;
+
+    private void Start()
+    {
+        if (!photonView.IsMine) { return; }
+        playerStatus.SetActive(false);
+    }
+
     void Update()
     {
+        if(photonView.IsMine) { return;}
 
+        Camera cameraToLookAt = Camera.main;
+        transform.LookAt(cameraToLookAt.transform);
     }
+
 }
