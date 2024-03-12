12d2a03eeafb874fec17e03bb9cd60d631f40177
Toninnn <126908275+Toninnn@users.noreply.github.com>
2024-03-12 16:29:12 -0300

    Att

--- CRIA MUNDO - Teste/Assets/GAME/Scripts/Missoes.cs
+++ CRIA MUNDO - Teste/Assets/GAME/Scripts/Missoes.cs
@@ -4,44 +4,67 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
+using System;
 
 public class Missoes : MonoBehaviourPunCallbacks
 {
     public Slider barra;
     public TextMeshProUGUI porcent;
     public float valorMax = 1000f;
+    PhotonView View;
+
+    [Range(0f, 1000f)]
     private float currentValue = 0f;
+
     public Button button;
+
+    [Range(0f, 100f)]
     private float porcentagem = 0f;
 
+    private float action = 0f;
 
-   
     public void Start()
     {
         barra.maxValue = valorMax;
         barra.value = currentValue;
-
     }
-
-   
 
     public void BarraUpdate()
     {
 
-        currentValue += 32;
+        if (photonView.IsMine)
+        {
+            photonView.RPC("somandoBarRPC", RpcTarget.AllBuffered, currentValue += 32);
+        }
+        
+       // currentValue += 32;
+       // somandoBarRPC();
         Debug.Log(currentValue);
+
         porcentagem = currentValue / 10;
+
         if(currentValue >= valorMax)
         {
             porcentagem = 100f;
         }
-        somandoBar();
+       
         porcent.text = string.Format("{0}%", porcentagem);
     }
 
+   /* private IEnumerator TweenTimer(float duration, Action<float> onTime = null, Action onEnd = null)
+    {
+        var _timer = 100f;
+        while (_timer <= duration)
+        {
+            _timer += Time.deltaTime;
+            yield return new WaitForSeconds(Time.deltaTime);
+            onTime?.Invoke(_timer);
+        }
+    }
+   */
+
     [PunRPC]
-    public void somandoBar()
+    public void somandoBarRPC()
     {
         
         barra.value = currentValue;
@@ -53,15 +76,5 @@ public class Missoes : MonoBehaviourPunCallbacks
 
     }   
 
-   
-
-    public void ValueChangeCheck()
-    {
-        
-        barra.value += 1f;
-        Debug.Log(barra.value);
-    }
-
-   
 
 }
